4e2fb108edb8f6b9a6b5ef898fb277ec1861481f
timmoooo <61495579+timmoooo@users.noreply.github.com>
2020-04-24 14:40:09 +0300

    kartan alku

---
+++ Ryhmä 8 projekti/JS/kartta.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let paikka = null;
+
+const map = L.map('map');
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function success(pos) {
+    paikka = pos.coords;
+    map.setView([paikka.latitude, paikka.longitude], 13);
+}
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${}`)
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
