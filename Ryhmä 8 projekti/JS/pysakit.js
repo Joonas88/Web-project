1d08deb1840cf55aab9c0fa8bdc57bced850c9b6
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-04-25 21:29:36 +0300

    Pysäkkihaku tehty uudelleen eri menetelmällä, sisältää nyt myös pysäkeiltä kulkevat linjat, sekä määränpäät.

--- Ryhmä 8 projekti/JS/pysakit.js
+++ Ryhmä 8 projekti/JS/pysakit.js
@@ -19,7 +19,6 @@ const options = {
 
 function success(pos) {
     paikka = pos.coords;
-    etsilinja(paikka);
 
     console.log(`Latitude: ${paikka.latitude}`);
     console.log(`Longitude: ${paikka.longitude}`);
@@ -39,9 +38,81 @@ const hakunappi = document.getElementById('hakunappi');
 hakunappi.addEventListener('click', napinpano);
 
 function napinpano() {
-    etsi(paikka);
+    //etsi(paikka);
+    pysakit(paikka);
 }
 
+function pysakit (crd) {
+    const hakuteksti = document.getElementById('hakukenttä').value;
+    console.log(hakuteksti);
+
+    const pysakkiKysely = {
+        query: `{
+    stopsByRadius(lat:${crd.latitude},lon:${crd.longitude},radius:${hakuteksti}) {
+      edges {
+        node {
+          stop { 
+            gtfsId 
+            name
+            lat
+            lon
+                patterns {
+      code
+      directionId
+      headsign
+      route {
+        gtfsId
+        shortName
+        longName
+        mode
+      }
+    }
+          }
+          distance
+        }
+      }
+    }
+}`
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+        },
+        body: JSON.stringify(pysakkiKysely),
+    };
+
+    fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', options).then(function (response) {
+        return response.json()
+    }).then(function (tulos) {
+        console.log(tulos);
+        for (let x = 0; x < tulos.data.stopsByRadius.edges.length; x++) {
+            console.log(tulos.data.stopsByRadius.edges[x].node.stop.name+' '+tulos.data.stopsByRadius.edges[x].node.distance+'m päässä');
+            const koordinaatit = {latitude: tulos.data.stopsByRadius.edges[x].node.stop.lat, longitude: tulos.data.stopsByRadius.edges[x].node.stop.lon};
+
+            //document.write('<br/>' + tulos.data.stopsByRadius.edges[x].node.stop.name+' '+tulos.data.stopsByRadius.edges[x].node.distance+'m päässä<br/>');
+            for (let i = 0; i < tulos.data.stopsByRadius.edges[x].node.stop.patterns.length; i++) {
+                console.log(tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].route.shortName + ' ' + tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].headsign);
+                const teksti=`<h3>${tulos.data.stopsByRadius.edges[x].node.stop.name}</h3><p>${tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].route.shortName}</p><p>${tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].headsign}</p>`;
+                //document.write(tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].route.shortName + ' ' + tulos.data.stopsByRadius.edges[x].node.stop.patterns[i].headsign + '<br/>');
+                lisaaMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop, tulos.data.stopsByRadius.edges[x].node.stop.patterns[i]);
+            }
+
+        }
+    });
+}
+
+function lisaaMarker(crd, teksti, hakutulos, hakutulos2) {
+    L.marker([crd.latitude, crd.longitude]).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+        id.innerHTML =hakutulos.name;
+        nimi.innerHTML =hakutulos2.route.shortName;
+        namn.innerHTML =hakutulos2.headsign;
+    });
+}
+
+/*
 function etsi(crd) {
     const hakuteksti = document.getElementById('hakukenttä').value;
     console.log(hakuteksti);
@@ -68,15 +139,9 @@ function etsi(crd) {
         console.log(error);
     });
 }
+ */
 
-function lisaaMarker(crd, teksti, hakutulos) {
-    L.marker([crd.latitude, crd.longitude]).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
-        id.innerHTML =hakutulos.LYHYTTUNNU;
-        nimi.innerHTML =hakutulos.NIMI1;
-        namn.innerHTML =hakutulos.NAMN1;
-    });
-}
-
+/*
 function etsilinja(crd) {
     const hakuteksti = document.getElementById('hakukenttä').value;
     console.log(hakuteksti);
@@ -87,7 +152,7 @@ function etsilinja(crd) {
         //console.log(hakutulokset.features[0].attributes);
 
         //const pysakit= document.querySelector('main');
-/*
+
         for (let x=0; x<hakutulokset.features.length; x++) {
 
             const koordinaatit = {latitude: hakutulokset.features[x].geometry.y, longitude: hakutulokset.features[x].geometry.x};
@@ -101,9 +166,13 @@ function etsilinja(crd) {
 
         }
 
- */
+
 
     }).catch(function (error) {
         console.log(error);
     });
 }
+
+ */
+
+
