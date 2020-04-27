e2be42e106e158bb7e6eaaaf95d1a51b9667c99a
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-04-27 23:06:10 +0300

    Karttaan lisätty Joonatanin tekemät markerit ja lisätty koodiin funktioita kaikille omille markereille, eli junat, bussit, metrot ja ratikat on omilla ikoneilla.

--- Ryhmä 8 projekti/JS/pysakit.js
+++ Ryhmä 8 projekti/JS/pysakit.js
@@ -3,6 +3,37 @@ const ylaLista = document.getElementById('pysakkiInfo');
 const lista = document.getElementById('data');
 const pysakkiCheck = document.getElementById('pysakit');
 pysakkiCheck.checked=true;
+let ajoneuvoId = null;
+
+const bussiIkoni = L.icon({
+    iconUrl: 'media/Bussicon.png',
+    iconSize: [40,40],
+    iconAnchor: [10,30],
+    popupAnchor: [10,-30]
+});
+
+const ratikkaIkoni = L.icon({
+    iconUrl: 'media/Sporaicon.png',
+    iconSize: [40,40],
+    iconAnchor: [10,30],
+    popupAnchor: [10,-30]
+});
+
+const junaIkoni = L.icon({
+    iconUrl: 'media/Junaicon.png',
+    iconSize: [40,40],
+    iconAnchor: [10,30],
+    popupAnchor: [10,-30]
+});
+
+const metroIkoni = L.icon({
+    iconUrl: 'media/Metroicon.png',
+    iconSize: [40,40],
+    iconAnchor: [10,30],
+    popupAnchor: [10,-30]
+});
+
+
 
 let paikka = null;
 
@@ -24,8 +55,7 @@ function success(pos) {
     console.log(`Longitude: ${paikka.longitude}`);
 
     map.setView([paikka.latitude, paikka.longitude], 13);
-
-    lisaaMarker(paikka, 'Minä olen tässä');
+    minaOlenTassa(paikka,'Minä olen tässä!');
 }
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -51,6 +81,7 @@ function napinpano() {
 
 }
 
+//Helsingin rautatieasmean koordinaatit: lat:60.171040,lon: 24.941957
 
 function pysakit (crd) {
     const pysakkiKysely = {
@@ -60,6 +91,7 @@ function pysakit (crd) {
         node {
           stop { 
             gtfsId 
+            vehicleMode
             name
             lat
             lon
@@ -91,16 +123,19 @@ function pysakit (crd) {
 
             const teksti=`<h4>${tulos.data.stopsByRadius.edges[x].node.stop.name+' '+tulos.data.stopsByRadius.edges[x].node.distance+'m päässä'}</h4>`;
 
-            lisaaMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop.gtfsId);
+            console.log(tulos.data.stopsByRadius.edges[x].node.stop.vehicleMode);
+
+            if (tulos.data.stopsByRadius.edges[x].node.stop.vehicleMode==='TRAM'){
+                sporaMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop.gtfsId);
+            } else if (tulos.data.stopsByRadius.edges[x].node.stop.vehicleMode==='RAIL') {
+                junaMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop.gtfsId);
+            } else if (tulos.data.stopsByRadius.edges[x].node.stop.vehicleMode==='SUBWAY'){
+                metroMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop.gtfsId);
+            } else {
+                bussiMarker(koordinaatit, teksti, tulos.data.stopsByRadius.edges[x].node.stop.gtfsId);
+            }
 
         }
-    });
-}
-
-function lisaaMarker(crd, teksti, pysakkiId) {
-    L.marker([crd.latitude, crd.longitude]).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
-        clear();
-        kulkuneuvot(pysakkiId)
     });
 }
 
@@ -121,16 +156,16 @@ function kulkuneuvot (pysakkiId) {
       realtimeState
       serviceDay
       headsign
-                      trip {
-                route {
-                gtfsId
-                  mode
-                  shortName
-                  longName
-                }
-              }
-    }
-  }  
+        trip {
+            route {
+            gtfsId
+            mode
+            shortName
+            longName
+            }
+        }
+      }
+    }  
 }`
     };
     const options = {
@@ -153,6 +188,7 @@ function kulkuneuvot (pysakkiId) {
             const aikaLeima = new Date((pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].serviceDay+pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].realtimeDeparture)*1000).toLocaleTimeString("fi-FI");
 
             console.log(pysakkiInfo.data.stop.name);
+            console.log(pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.gtfsId);
             console.log(pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.shortName+' '+pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].headsign+' '+aikaLeima);
 
             let maaranpaa = null;
@@ -163,8 +199,10 @@ function kulkuneuvot (pysakkiId) {
                 maaranpaa=pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].headsign;
             }
 
-            tietojenTulostus(pysakkiInfo.data.stop.name,pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.shortName, maaranpaa, aikaLeima, pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.gtfsId);
+            ajoneuvoId=pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.gtfsId;
 
+            tietojenTulostus(pysakkiInfo.data.stop.name,pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.shortName, maaranpaa, aikaLeima, pysakkiInfo.data.stop.stoptimesWithoutPatterns[x].trip.route.gtfsId);
+            reittiTiedot(ajoneuvoId);
         }
 
     });
@@ -173,6 +211,40 @@ function kulkuneuvot (pysakkiId) {
 function tietojenTulostus(pysakinNimi, linjaNumero, maaranpaa, lahtoAika, reittiID) {
     pysakki.innerHTML=pysakinNimi;
     lista.innerHTML+=lahtoAika+'<br/>'+`<a href="https://reittiopas.hsl.fi/linjat/${reittiID}" target="_blank">${linjaNumero} ${maaranpaa}</a><br/><br/>`;
+}
+
+function minaOlenTassa(crd, teksti) {
+    L.marker([crd.latitude, crd.longitude]).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+
+    });
+}
+
+function bussiMarker(crd, teksti, pysakkiId) {
+    L.marker([crd.latitude, crd.longitude], {icon: bussiIkoni}).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+        clear();
+        kulkuneuvot(pysakkiId)
+    });
+}
+
+function sporaMarker(crd, teksti, pysakkiId) {
+    L.marker([crd.latitude, crd.longitude], {icon: ratikkaIkoni}).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+        clear();
+        kulkuneuvot(pysakkiId)
+    });
+}
+
+function junaMarker(crd, teksti, pysakkiId) {
+    L.marker([crd.latitude, crd.longitude], {icon: junaIkoni}).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+        clear();
+        kulkuneuvot(pysakkiId)
+    });
+}
+
+function metroMarker(crd, teksti, pysakkiId) {
+    L.marker([crd.latitude, crd.longitude], {icon: metroIkoni}).addTo(map).bindPopup(teksti).openPopup().on('click', function () {
+        clear();
+        kulkuneuvot(pysakkiId)
+    });
 }
 
 function clear() {
