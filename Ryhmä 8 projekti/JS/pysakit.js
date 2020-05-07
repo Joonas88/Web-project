a645d850ec1631ef15659d59d09f87e88e8cb68e
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-05-07 16:14:57 +0300

    Tulostutsen taustaväriä optimoitu. Video upotettu sivustolle. Värimaailmaa paranneltu

--- Ryhmä 8 projekti/JS/pysakit.js
+++ Ryhmä 8 projekti/JS/pysakit.js
@@ -684,6 +684,7 @@ function trainSchedule(zoneID, asemaTunnus, junaTietolista) {//Funktiolla haetaa
     });
 }
 
+
 function junaAikatauluKoostus(junaTietolista, lahtoaika, linjaID, raide) {//Funktiolla koostetaan kahden eri rajapinnan tiedot yhdeksi aikataululistaksi
     let dateStr = null, maaranpaa = null, paivamaara = null;
     dateStr = lahtoaika;
